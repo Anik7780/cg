ffb773184c051de79cac5c31b91af2fc504adaad
Anik7780 <anik8q@gmail.com>
2025-03-29 15:05:29 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,18 +2,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// তোমার Firebase config
+// Firebase কনফিগারেশন সেট করুন
 const firebaseConfig = {
   apiKey: "AIzaSyB-D2z6yLSVolxXmVeAd5xG03zC0pzkpU8",
   authDomain: "cgbazar-a40c0.firebaseapp.com",
   projectId: "cgbazar-a40c0",
-  storageBucket: "cgbazar-a40c0.firebasestorage.app",
+  storageBucket: "cgbazar-a40c0.appspot.com",
   messagingSenderId: "33145540262",
   appId: "1:33145540262:web:2820f803fb1548458f6e67",
   measurementId: "G-96NYF34CWB"
 };
 
-// Initialize Firebase
+// Firebase Initialize
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
